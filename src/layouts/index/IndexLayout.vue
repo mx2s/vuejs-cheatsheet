5bab0b9f842b41fae42af7eac3704c662115f8bd
Max Kovalenko <mxss1998@yandex.ru>
2019-10-12 20:08:24 -0400

    dynamic component loading

--- src/layouts/index/IndexLayout.vue
+++ src/layouts/index/IndexLayout.vue
@@ -1,13 +1,21 @@
 <template>
-    <p>Index layout</p>
+    <p>External component content. {{ exampleData }} </p>
+    <el-button type="primary">Primary button example</el-button>
 </template>
 
 <script>
-    export default {
-        name: "IndexLayout"
+    module.exports = {
+        data: function () {
+            return {
+                exampleData: 'data from component'
+            }
+        }
     }
 </script>
 
 <style scoped>
-
+    p {
+        font-size: 2em;
+        text-align: center;
+    }
 </style>
